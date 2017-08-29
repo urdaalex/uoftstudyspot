8733b3bb3234c4596dd4c753fa033a32ec7dfb87
Alex Urda <URDAA@elearning.ssddomain.opg.com>
2017-08-29 09:20:42 -0400

    config change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -371,7 +371,7 @@ module.exports = {
     }),
     new ProgressPlugin(),
     new HtmlWebpackPlugin({
-      "template": "./src\\index.html",
+      "template": "./src/index.html",
       "filename": "./index.html",
       "hash": false,
       "inject": true,
